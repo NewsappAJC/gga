3dca28e17a94933ad70d93b2586eb394e4ed55dc
John Perry <jgperry@ajc.com>
2014-02-06 17:02:58 -0500

    updated Gruntfile with new S3 bucket

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,42 +123,46 @@ module.exports = function(grunt) {
       }
     },
     s3: {
-      key: "<%= aws.key %>",
-      secret: "<%= aws.secret %>",
-      bucket: "<%= aws.bucket %>",
-      access: "public-read",
-      gzip: true,
-      debug: false,
-      maxOperations: 10,
-      upload: [
-        { src: 'build/index.html', dest: 'index.html' },
-        { src: 'build/scripts/app.js', dest: 'scripts/app.js' },
-        { src: 'build/scripts/require_main.built.js', dest: 'scripts/require_main.built.js' },
-        { src: 'build/scripts/require_main.js', dest: 'scripts/require_main.js' },
-        { src: 'build/scripts/underscore.js', dest: 'scripts/underscore.js' },
-        { src: 'build/scripts/underscore-amd.js', dest: 'scripts/underscore-amd.js' },
-        { src: 'build/scripts/lib/require.js', dest: 'scripts/lib/require.js' },
-        { src: 'build/scripts/entities/*', dest: 'scripts/entities/' },
-        { src: 'build/scripts/apps/bills/bills_app.js', dest: 'scripts/apps/bills/bills_app.js' },
-        { src: 'build/scripts/apps/bills/show/*', dest: 'scripts/apps/bills/show/' },
-        { src: 'build/scripts/apps/members/members_app.js', dest: 'scripts/apps/members/members_app.js' },
-        { src: 'build/scripts/apps/members/list/*', dest: 'scripts/apps/members/list/' },
-        { src: 'build/scripts/apps/members/show/*', dest: 'scripts/apps/members/show/' },
-        { src: 'build/scripts/apps/watched_bills/watched_bills_app.js', dest: 'scripts/apps/watched_bills/watched_bills_app.js' },
-        { src: 'build/scripts/apps/watched_bills/list/*', dest: 'scripts/apps/watched_bills/list/' },
-        { src: 'build/scripts/apps/watched_bills/show/*', dest: 'scripts/apps/watched_bills/show/' },
-        { src: 'build/scripts/apps/welcome/*', dest: 'scripts/apps/welcome/'},
-        { src: 'build/scripts/apps/about/*', dest: 'scripts/apps/about/'},
-        { src: 'build/scripts/common/*', dest: 'scripts/common/'},
+      options: {
+        key: "<%= aws.key %>",
+        secret: "<%= aws.secret %>",
+        bucket: "<%= aws.bucket %>",
+        access: "public-read",
+        gzip: true,
+        debug: false,
+        maxOperations: 10
+      },
+      dev: {
+        upload: [
+          { src: 'build/index.html', dest: 'index.html' },
+          { src: 'build/scripts/app.js', dest: 'scripts/app.js' },
+          { src: 'build/scripts/require_main.built.js', dest: 'scripts/require_main.built.js' },
+          { src: 'build/scripts/require_main.js', dest: 'scripts/require_main.js' },
+          { src: 'build/scripts/underscore.js', dest: 'scripts/underscore.js' },
+          { src: 'build/scripts/underscore-amd.js', dest: 'scripts/underscore-amd.js' },
+          { src: 'build/scripts/lib/require.js', dest: 'scripts/lib/require.js' },
+          { src: 'build/scripts/entities/*', dest: 'scripts/entities/' },
+          { src: 'build/scripts/apps/bills/bills_app.js', dest: 'scripts/apps/bills/bills_app.js' },
+          { src: 'build/scripts/apps/bills/show/*', dest: 'scripts/apps/bills/show/' },
+          { src: 'build/scripts/apps/members/members_app.js', dest: 'scripts/apps/members/members_app.js' },
+          { src: 'build/scripts/apps/members/list/*', dest: 'scripts/apps/members/list/' },
+          { src: 'build/scripts/apps/members/show/*', dest: 'scripts/apps/members/show/' },
+          { src: 'build/scripts/apps/watched_bills/watched_bills_app.js', dest: 'scripts/apps/watched_bills/watched_bills_app.js' },
+          { src: 'build/scripts/apps/watched_bills/list/*', dest: 'scripts/apps/watched_bills/list/' },
+          { src: 'build/scripts/apps/watched_bills/show/*', dest: 'scripts/apps/watched_bills/show/' },
+          { src: 'build/scripts/apps/welcome/*', dest: 'scripts/apps/welcome/'},
+          { src: 'build/scripts/apps/about/*', dest: 'scripts/apps/about/'},
+          { src: 'build/scripts/common/*', dest: 'scripts/common/'},
 
-        // { src: 'build/images/cropped/*', dest: 'images/cropped/' },
-        // { src: 'build/images/datatables/*', dest: 'images/datatables/' },
-        // { src: 'build/images/mugs/*', dest: 'images/mugs/' },
-        { src: 'build/style/*', dest: 'style/' },
-        { src: 'build/style/images/*', dest: 'style/images/' },
-        { src: 'build/style/dist/bootstrap.css', dest: 'style/dist/bootstrap.css' },
-        { src: 'build/images/*', dest: 'images/' }
-      ]
+          // { src: 'build/images/cropped/*', dest: 'images/cropped/' },
+          // { src: 'build/images/datatables/*', dest: 'images/datatables/' },
+          // { src: 'build/images/mugs/*', dest: 'images/mugs/' },
+          { src: 'build/style/*', dest: 'style/' },
+          { src: 'build/style/images/*', dest: 'style/images/' },
+          { src: 'build/style/dist/bootstrap.css', dest: 'style/dist/bootstrap.css' },
+          { src: 'build/images/*', dest: 'images/' }
+        ]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
