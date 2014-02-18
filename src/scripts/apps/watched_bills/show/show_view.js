717c4c25f91e9bb0d1753c46c7435288c3b11c6f
John Perry <jgperry@ajc.com>
2014-02-18 17:24:58 -0500

    finished adding predictions to watched bill list

--- src/scripts/apps/watched_bills/show/show_view.js
+++ src/scripts/apps/watched_bills/show/show_view.js
@@ -15,7 +15,17 @@ define(["app"], function(GeneralAssemblyApp) {
     View.CategoryItemView = Marionette.ItemView.extend({
       template: "#category-item-template",
       tagName: "li",
-      className: "list-group-item"
+      className: "list-group-item",
+      onShow: function() {
+        var prob = this.model.get("predictions") * 100;
+        var id = this.model.get("bill_id");
+        var obj = $("#" + id);
+        $(obj).addClass( function() {
+          return prob <= 20 ? "alert-danger"  :
+                 prob <  80 ? "alert-warning" :
+                 "alert-success";
+        });
+      }
     });
 
     View.CategoryCollectionView = Marionette.CollectionView.extend({
