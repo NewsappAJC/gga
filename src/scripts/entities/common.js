1e5cc49b7adced48a157338b1bc426e4c5b4abf2
John Perry <jgperry@ajc.com>
2014-02-24 14:28:49 -0500

    added crossoverday status to bill detail page

--- src/scripts/entities/common.js
+++ src/scripts/entities/common.js
@@ -1,7 +1,7 @@
 define(["app"], function(GeneralAssemblyApp) {
   GeneralAssemblyApp.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
-    Entities.api_base = "http://ajcgga-api.herokuapp.com/api/";
-    // Entities.api_base = "http://localhost:3000/api/";
+    // Entities.api_base = "http://ajcgga-api.herokuapp.com/api/";
+    Entities.api_base = "http://localhost:3000/api/";
     Entities.FilteredCollection = function(options) {
       var original = options.collection;
       var filtered = new original.constructor();
