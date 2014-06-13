1ac279d2bccbde21a3da0d438bccc40497c2373a
Mike Pennisi <mike@mikepennisi.com>
2014-06-13 15:39:03 -0400

    Write logic to navigate to "member" page
    Actual integration tests pending.

--- test/integration/my_first_test.js
+++ test/integration/my_first_test.js
@@ -56,6 +56,43 @@ describe('homepage', function() {
         });
     });
 
+    describe('member page', function() {
+      beforeEach(function() {
+        var firstMemberThumbnail;
+
+        // TODO: Conditionally increase test timeout only when web requests
+        // are not being mocked.
+        this.timeout(10000);
+
+        return driver
+          .findElement(webdriver.By.css(selectors.layouts.members.thumbnail))
+          .then(function(el) {
+            firstMemberThumbnail = el;
+            return driver.wait(function() {
+              console.log('checking if element is displayed');
+              return firstMemberThumbnail.isDisplayed();
+            });
+          }).then(function() {
+            // TODO: Remove this workaround when application is fixed
+            // to not scroll in response to the "members" route.
+            return driver.executeScript(function() {
+              scrollTo(0, 0);
+            });
+          }).then(function(data) {
+            return firstMemberThumbnail.click();
+          })
+          .then(function() {
+            return driver.wait(function() {
+              return driver.isElementPresent(
+                webdriver.By.css(selectors.layouts.member.region)
+              );
+            });
+          });
+      });
+
+      it('behaves correctly');
+    });
+
     describe('filtering', function() {
       this.timeout(8000);
 
