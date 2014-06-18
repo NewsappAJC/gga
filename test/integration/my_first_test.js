e6785cb7cf7683dffaa504a191f0986090ad993b
Mike Pennisi <mike@mikepennisi.com>
2014-06-18 13:08:18 -0400

    Add more tests for "bills" section

--- test/integration/my_first_test.js
+++ test/integration/my_first_test.js
@@ -176,6 +176,66 @@ describe('homepage', function() {
               });
           });
       });
+
+      describe('bill search', function() {
+        var billId = '1151';
+
+        it('correctly returns requested house bill', function() {
+          return driver.findElement(
+            webdriver.By.css(selectors.layouts.bills.search.documentType)
+          ).then(function(selectElement) {
+            return selectElement.sendKeys('house bills');
+          }).then(function() {
+            return driver.findElement(
+              webdriver.By.css(selectors.layouts.bills.search.searchform)
+            );
+          }).then(function(numberInput) {
+            return numberInput.sendKeys(billId)
+              .then(function() {
+                return numberInput.submit();
+              });
+          }).then(function() {
+            return driver.wait(function() {
+              return driver.isElementPresent(
+                webdriver.By.css(selectors.layouts.bill.region)
+              );
+            }, 3000);
+          }).then(function() {
+            return driver.findElement(
+              webdriver.By.css(selectors.layouts.bill.title)
+            );
+          }).then(function(element) {
+            return element.getText();
+          }).then(function(text) {
+            assert(
+              text.indexOf(billId) > -1, 'Expected bill ID present on page.'
+            );
+          });
+        });
+
+        it('does not return house bills when searching senate bills');
+      });
+
+      it('every category displays a positive number of watched bills', function() {
+        var countRe = /\b(\d+)\s*bills?\b/i;
+
+        return driver.findElements(
+          webdriver.By.css(selectors.layouts.bills.thumbnail)
+        ).then(function(elements) {
+          assert.equal(elements.length, 10);
+
+          return webdriver.promise.all(elements.map(function(element) {
+            return element.getText();
+          })).then(function(texts) {
+            texts.forEach(function(text) {
+              var match = text.match(countRe);
+
+              assert(match, 'Displays some number of bills');
+              assert(parseInt(match[0], 10) > 0, 'Displays a positive number of bills');
+            });
+          });
+        });
+      });
     });
   });
 });
